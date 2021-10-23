a964173bfaf3c48a44cad1935c0b8ae555c3ff04
patricnox <hello@patricnox.info>
2021-10-23 13:47:41 +0200

    Change description element to a p tag for site description

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>WCL Lookup Tool</h1>
-        <description>A simple tool to lookup a characters latest info.</description>
+        <p>A simple tool to lookup a characters latest info.</p>
       </header>
       <QueryForm></QueryForm>
     </div>
