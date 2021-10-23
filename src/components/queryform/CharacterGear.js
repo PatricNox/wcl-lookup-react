5fe00ce808b5eefb77cad00bb49a9ced87869915
patricnox <hello@patricnox.info>
2021-10-23 20:36:32 +0200

    Implement itemColorByQuality

--- src/components/queryform/CharacterGear.js
+++ src/components/queryform/CharacterGear.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
+function itemColorByQuality(quality) {
+  switch (quality) {
+    case "common":
+      return 'text-white';
+    case "uncommon":
+      return 'text-green-500';
+    case "rare":
+      return 'text-blue-500';
+    case "epic":
+      return 'text-purple-500';
+    case "legendary":
+      return 'text-yellow-500';
+    case "artifact":
+      return 'text-yellow-200';
+    case "heirloom":
+      return 'text-blue-300';
+    default:
+      // Poor
+      return 'text-gray-500';
+  }
+}
 class CharacterGear extends React.Component {
   render() {
     const assetUrl = process.env.REACT_APP_WCL_ASSETS;
@@ -8,7 +29,7 @@ class CharacterGear extends React.Component {
       <div className="flex mt-4 p-2 w-2/3 m-auto border border-yellow-100 text-left">
           <div className="flex flex-col">
             {items.map(item => (
-              <div className="flex text-blue-300 ">
+              <div className="flex">
                 <div class="ml-1">
                   <img
                     src={ assetUrl + item.icon }
@@ -16,7 +37,7 @@ class CharacterGear extends React.Component {
                     width="40" />
                 </div>
                 <div className="mt-2 ml-4">
-                  <p>{item.name}</p>
+                  <p className={itemColorByQuality(item.quality)}>{item.name}</p>
                 </div>
               </div>
             ))}
