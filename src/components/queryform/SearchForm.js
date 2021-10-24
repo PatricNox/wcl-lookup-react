f5bcf7afcb4bfbd07662fc8c8f9e77c9c20e3369
patricnox <hello@patricnox.info>
2021-10-24 12:17:33 +0200

    Add placeholder to inputs & fix react state update on an unmounted component

--- src/components/queryform/SearchForm.js
+++ src/components/queryform/SearchForm.js
@@ -2,15 +2,15 @@ import React from 'react';
 import WCLApi from '../../api/WCLApi';
 
 class SearchForm extends React.Component {
-    constructor() {
-        super();
-        this.state = { query: '', realm: '', region: 'eu', loading: false, inProgress: true, message: '' };
-        this.handleQueryChange  = this.handleQueryChange.bind(this);
-        this.handleRealmChange  = this.handleRealmChange.bind(this);
-        this.handleRegionChange  = this.handleRegionChange.bind(this);
-        this.handleSubmit  = this.handleSubmit.bind(this);
-        this.updateProgressState  = this.updateProgressState.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = { query: '', realm: '', region: 'eu', loading: false, inProgress: true, message: '' };
+    this.handleQueryChange  = this.handleQueryChange.bind(this);
+    this.handleRealmChange  = this.handleRealmChange.bind(this);
+    this.handleRegionChange  = this.handleRegionChange.bind(this);
+    this.handleSubmit  = this.handleSubmit.bind(this);
+    this.updateProgressState  = this.updateProgressState.bind(this);
+  }
   handleQueryChange(event) {
     this.updateProgressState();
     this.setState({query: event.target.value});
@@ -23,7 +23,6 @@ class SearchForm extends React.Component {
     this.setState({region: event.target.value});
     this.updateProgressState();
   }
-
   updateProgressState() {
     if (this.state.query && this.state.realm && this.state.region) {
       this.setState({ inProgress: false });
@@ -31,14 +30,24 @@ class SearchForm extends React.Component {
   }
   handleSubmit(event) {
     event.preventDefault();
+
+    let charData = null;
     this.setState({ loading: true, message: 'loading..' });
+
+    // Query backend using superpromise.
     WCLApi.parses.lookup(this.state.query, this.state.realm, this.state.region).then((data) => {
-      if (data.characterName) this.props.onQuery(data)
+      // If we got a character name, store the data in a scoped variable.
+      if (data.characterName) charData = data;
       else this.setState({message: 'Character not found'})
       this.setState({ query: '', realm: '', region: 'eu' });
     }).finally(() => {
       this.setState({ loading: false });
-    });
+
+      // Finally, bubble to parentComponent.
+      // Must be the last event due to we can't perform a react state
+      // update on an unmounted component. (unmounts in parentComponent if we got data)
+      if (charData) this.props.onQuery(charData);
+    }).catch((e) => console.log(e));
   }
   render() {
     const isLoading = this.state.loading;
@@ -50,10 +59,12 @@ class SearchForm extends React.Component {
                 <input type="text"
                 className="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
                   value={this.state.query}
+                  placeholder="Character"
                   onChange={this.handleQueryChange}
                 />
                 <input className="ml-2 px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200" type="text"
                   value={this.state.realm}
+                  placeholder="Realm (Server)"
                   onChange={this.handleRealmChange}
                 />
                 <select className="ml-2 px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200" value={this.state.region} onChange={this.handleRegionChange}>
